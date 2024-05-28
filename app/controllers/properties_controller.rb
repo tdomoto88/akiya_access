9cd4f1881e239ee6e9de9ee7e98246871527f5e5
Hugh Clayton <hughgrassbyclayton@hotmail.com>
2024-05-28 16:20:26 +1000

    updates

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,16 +1,28 @@
 class PropertiesController < ApplicationController
   def index
+    @properties = Property.all
   end
 
   def show
+    @properties = Property.find(params[:id])
   end
 
   def new
+    @property = Property.new
   end
 
   def create
+    @property = Property.new(property_params)
+    @property.save
+    redirect_to root_path
   end
 
   def destroy
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:price, :age, :address, :bedrooms, :description, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :properties do
     resources :bookings
   end
-  root to: "pages#home"
+  root to: "properties#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
