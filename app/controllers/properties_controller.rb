fbdaac9a31c011f1a314f4596d27ba5eeeff5b26
Hugh Clayton <hughgrassbyclayton@hotmail.com>
2024-05-29 19:02:53 +1000

    alot of shit

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,5 @@
 class PropertiesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @properties = Property.all
   end
@@ -24,9 +25,14 @@ class PropertiesController < ApplicationController
     redirect_to root_path
   end
 
+  def user_properties
+    @properties = Property.where(user: current_user)
+  end
+
+
   private
 
   def property_params
-    params.require(:property).permit(:price, :age, :address, :bedrooms, :description, :user_id)
+    params.require(:property).permit(:price, :age, :city, :address, :bedrooms, :description, :user_id, :photo)
   end
 end
