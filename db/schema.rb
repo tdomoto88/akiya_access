ae07872174699182b9e275da94ee8e73457cef6a
Hugh Clayton <hughgrassbyclayton@hotmail.com>
2024-05-30 16:47:27 +0800

    dashboard stuff, booking confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_042914) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_050326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_042914) do
     t.bigint "property_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "accepted"
     t.index ["property_id"], name: "index_bookings_on_property_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
