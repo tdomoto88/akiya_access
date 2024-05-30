a8550327b59c9ff845809cd53c889be7e14a1c66
Lance Masina <lance@lancemasina.com>
2024-05-30 16:02:08 +0800

    styling sidebar and prefecture validations for add new property complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_05_30_032652) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,12 +63,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_30_032652) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.float "latitude"
     t.float "longitude"
     t.string "city"
     t.string "prefecture"
-
     t.index ["user_id"], name: "index_properties_on_user_id"
   end
 
