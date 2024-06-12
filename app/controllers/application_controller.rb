6ede8c398da83d53cc6c78412ab3df27c0cb157a
Hugh Clayton <hughgrassbyclayton@hotmail.com>
2024-06-12 13:11:00 +0800

    become an owner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :dob, :is_owner])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :dob])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :dob, :is_owner])
   end
 end
