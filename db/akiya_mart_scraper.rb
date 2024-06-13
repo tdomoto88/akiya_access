4da9b3d93e17c98e8bd46fae9ffb1a4babd9bb4f
Tsuyoshi Domoto <tdomoto88@gmail.com>
2024-06-13 14:56:20 +0800

    footer

--- db/akiya_mart_scraper.rb
+++ db/akiya_mart_scraper.rb
@@ -12,19 +12,29 @@ User.create!(first_name: "Hugh", last_name: "Clayton", email: "hughgrassbyclayto
 User.create!(first_name: "Tsuyoshi", last_name: "Domoto", email: "tdomoto88@gmail.com", password: "123456", is_owner: false)
 User.create!(first_name: "Lance", last_name: "Masina", email: "lance@lancemasina.com", password: "123456", is_owner: false)
 User.create!(first_name: "Xavier", last_name: "Ropero", email: "xavier.ropero@gmail.com", password: "123456", is_owner: false)
-
-properties.each do |property|
+puts properties.count
+properties_skipped = 0
+properties.each_with_index do |property, index|
+  puts index
   url = "https://www.akiya-mart.com/listings/id/#{property['listing_id']}?currency=usd"
   serialized_details = URI.open(url).read
   details = JSON.parse(serialized_details)['results']
   puts property['prefecture']
+
+  geocoded_city = Geocoder.search([details['lat'], details['lon']])
+  if geocoded_city.empty?
+    puts "skipped.."
+    properties_skipped += 1
+    next
+  end
+
   new_property = Property.new(age: property['construction_year'],
   prefecture: property['prefecture'].capitalize,
   price: property['price_foreign'],
   address: property['translated_address'],
   latitude: details['lat'],
   longitude: details['lon'],
-  city: Geocoder.search([details['lat'], details['lon']]).first.city,
+  city: geocoded_city.first.city,
   bedrooms: rand(1..6),
   bathrooms: rand(1..3),
   description: details['llm_description'],
@@ -39,6 +49,7 @@ properties.each do |property|
   end
 end
 
+puts "properties skipped: #{properties_skipped}"
 
 
 
